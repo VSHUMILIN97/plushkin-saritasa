1786582703a5e3549dbed37dd30ba44f5737e79e
Pavelev Ivan <ivan.saritasa.com>
2018-08-03 11:44:28 +0500

    Updated filemanager fields

--- FileManager.py
+++ FileManager.py
@@ -1,5 +1,9 @@
 class FileManager:
 
-    def __init__(self, clone_groups, exceptions):
+    def __init__(self, clone_groups, exceptions,
+                 sc_files_num, sc_fold_num):
         self.clone_groups = dict(clone_groups)  # dict: {group_id:[file_clones]}
         self.exceptions = exceptions  # dict: {file:exception}
+        self.scanned_files_num = sc_files_num  #
+        self.scanned_folders_num = sc_fold_num
+        self.duplications_num = len(clone_groups)
