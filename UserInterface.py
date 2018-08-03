14d5ea443d5da71c289e6ba6c000ba8c3974ba92
Silver3310 <epifanov998@mail.ru>
2018-08-03 15:05:20 +0700

    Add UserInterface module

---
+++ UserInterface.py
@@ -0,0 +1,133 @@
+import os
+import time
+
+
+class UserInterface:
+
+    def __init__(self, search_report):
+
+        self.search_report = search_report
+
+        self.big_line = \
+            '---------------------------------------------------------'
+
+        self.removed_files = 0
+        self.errors = 0
+        self.size_cleaned = 0
+
+    def exceptions_information(self):
+
+        print("The next files can't be read:")
+
+        for file in self.search_report.exceptions:
+            print(file)
+
+    def general_information(self):
+
+        print(
+            'Scan report for folder:',
+            self.search_report.name_scanned_folder
+        )
+        print(
+            'Files scanned:',
+            self.search_report.scanned_files_num
+        )
+        print(
+            'Folders scanned:',
+            self.search_report.scanned_folders_num
+        )
+        print(
+            'Duplications found:',
+            self.search_report.duplications_num
+        )
+
+    def show_search_report(self):
+
+        self.general_information()
+
+        self.exceptions_information()
+
+        for group_id, file_clones in self.search_report.clone_groups.items():
+
+            print(self.big_line)
+
+            size = os.path.getsize(file)
+            print(f'size: {size} bytes')
+
+            date = time.ctime(os.path.getctime(file))
+
+            for file in file_clones:
+                print(f'[{file.index()}]: {file} (Created: {date})')
+
+            print(self.big_line)
+
+    def show_cleaning_input(self, group_index):
+
+        self.general_information()
+
+        self.exceptions_information()
+
+        print(self.big_line)
+        print('CLEANING started')
+        print(self.big_line)
+
+        for file in self.search_report.clone_groups[group_index]:
+            print(f'[{file.index()}]: {file} (Created: {date})')
+
+        file_to_keep = input(
+            'Choose file to keep by entering '
+            'its number or press enter to skip it'
+        )
+
+        print(
+            f'File'
+            f' {self.search_report.clone_groups[group_index][file_to_keep]}'
+            f' was kept'
+        )
+
+        what_to_delete = (
+            self.search_report.clone_groups[group_index],
+            file_to_keep
+        )
+
+        return what_to_delete
+
+    def report(
+            self,
+            cleaner_report,
+    ):
+
+        print("Errors occurred:")
+        for error in cleaner_report[0]:
+            print(error)
+
+        print('Files removed:', cleaner_report[1])
+        print('Errors:', cleaner_report[3])
+        print('Size_cleaned:', cleaner_report[2])
+
+        self.removed_files += cleaner_report[1]
+        self.size_cleaned += cleaner_report[2]
+        self.errors += cleaner_report[3]
+
+    def overall(self):
+
+        print(self.big_line)
+        print('CLEANING finished')
+        print(self.big_line)
+
+        print('Files removed:', self.removed_files)
+        print('Errors:', self.errors)
+        print('Size_cleaned:', self.size_cleaned)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
