fa7535bd15b15faf766600cc9380ca22877e4b7e
vshumilin97 <vshumilin1488@gmail.com>
2018-08-03 12:46:08 +0500

    Add main logic of the app
    Make argument parser via click.
    Set a check for the incorrect path.

--- plushkin.py
+++ plushkin.py
@@ -1,2 +1,31 @@
-if __name__ == '__main__':
+import os
+import click
+import sys
+
+
+@click.command()
+@click.option('-d', '--delete',
+              is_flag=True,
+              help='Delete duplicates (Launch interactive cmd interface')
+@click.argument('path')
+def plushkin(path, delete):
+    if os.path.isdir(path):
+        print('Searcher look-up')
+        if delete:
+            for _ in range(3):
+                print('Interface and cleaner')
+    else:
+        raise DirectoryNotFoundException('Path not found')
+
+
+class DirectoryNotFoundException(BaseException):
     pass
+
+
+def excepthook(type, value, traceback):
+    print(value)
+
+
+if __name__ == '__main__':
+    sys.excepthook = excepthook
+    plushkin()
