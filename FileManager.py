ef856cdb5f1e9d4a2b8665d599d44c60c990bdc6
Pavelev Ivan <ivan.saritasa.com>
2018-08-03 12:36:54 +0500

    Files exceptions changed to set

--- FileManager.py
+++ FileManager.py
@@ -3,7 +3,7 @@ class FileManager:
     def __init__(self, clone_groups, exceptions,
                  sc_files_num, sc_fold_num, fold_name):
         self.clone_groups = dict(clone_groups)  # dict: {group_id:[file_clones]}
-        self.exceptions = exceptions  # dict: {file:exception}
+        self.exceptions = exceptions  # set: {file}
         self.scanned_files_num = sc_files_num  #
         self.scanned_folders_num = sc_fold_num
         self.duplications_num = len(clone_groups)
